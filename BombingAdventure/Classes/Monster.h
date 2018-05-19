53363a33d210c73d7d8ae826d41bfa5a4e6e4f8c
Brando Zhang <347830597@qq.com>
2018-05-19 12:34:49 +0800

    Add a naive class Monster
    Monster inherits from class Player, it is the enemy.
    Human player should kill all the monsters to win the game.

---
+++ BombingAdventure/Classes/Monster.h
@@ -0,0 +1,41 @@
+//
+// Created by Brando Zhang on 2018/5/16.
+//
+
+#ifndef BOMBING_ADVENTURE_MONSTER_H
+#define BOMBING_ADVENTURE_MONSTER_H
+
+#include "cocos2d.h"
+#include "Player.h"
+
+USING_NS_CC;
+//enum direction { MOVE_UP, MOVE_DOWN, MOVE_LEFT, MOVE_RIGHT };
+
+/* Following are the attributes of Monster   */
+const int MONSTER_DEFAULT_HP = 2;
+const int MONSTER_DEFAULT_AVAILABLE_BOMBS = 10;
+const float MONSTER_DEFAULT_MOVING_SPEED = 8.0f;
+
+class Monster : public Player {
+public:
+    /* Constructor  */
+    Monster();
+    /* Destructor   */
+    ~Monster();
+
+    CREATE_FUNC(Monster);
+
+    /* Initiate Method  */
+    virtual bool init();
+
+    /* Method: get_new_direction */
+    int get_new_direction();
+    /* Method: move_forward  */
+//    void move_forward(int new_direction);
+
+private:
+    enum direction { MOVE_UP, MOVE_DOWN, MOVE_LEFT, MOVE_RIGHT };
+
+};
+
+#endif //BOMBING_ADVENTURE_MONSTER_H
