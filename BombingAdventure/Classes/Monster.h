a8a210370257e24437aee117d9e88358458b274e
Brando Zhang <347830597@qq.com>
2018-05-19 22:53:43 +0800

    Add: Monster can check whether it collied with player now.

--- BombingAdventure/Classes/Monster.h
+++ BombingAdventure/Classes/Monster.h
@@ -28,10 +28,14 @@ public:
     /* Initiate Method  */
     virtual bool init();
 
+    bool collided_with(Player * player);
+
     /* Method: get_new_direction */
     int get_new_direction();
+
     /* Method: move_forward  */
 //    void move_forward(int new_direction);
+
 
 private:
     enum direction { MOVE_UP, MOVE_DOWN, MOVE_LEFT, MOVE_RIGHT };
